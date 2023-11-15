adc3465e24fa84b8e20bcb0ea28025bb9014916d
dctxf <dctmz@qq.com>
2023-11-15 09:59:13 +0800

    init

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,21 +4,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { FC } from 'react';
 import { Link } from 'react-router-dom';
-
-const Item: FC<{ title: React.ReactNode; to: string }> = ({ title, to }) => {
-  return (
-    <li className='sm:w-full md:w-1/2 xl:w-1/4'>
-      <Link
-        to={to}
-        className='p-4 text-blue-500 bg-slate-50 hover:bg-slate-100 block'
-      >
-        {title}
-      </Link>
-    </li>
-  );
-};
 
 const items = [
   {
